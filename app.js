8bbc8129a31e6183c1e3e42dddad87dc3607c8a7
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-04-03 13:45:22 +0100

    I finished styling the page

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const range = document.getElementById("range"),
+  switchBtn = document.getElementById("switch"),
   price = document.getElementById("price");
 range.addEventListener("input", (e) => {
   const val = range.value;
   price.textContent = `$${Number(val).toFixed(2)}`;
   // Sliding background
   e.target.style.backgroundSize = ((val - 0) * 100) / (100 - 0) + "% 100%";
+});
+//? ((val - 0) * 100) / (100 - 0)% 100%
+switchBtn.addEventListener("click", (e) => {
+  console.log(e.target.checked);
 });
